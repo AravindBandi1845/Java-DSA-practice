19f129bbe0d6701fc486815468bd0f188a3a2219
Aravind Bandi <aravind.bandi1845@gmail.com>
2026-03-23 17:11:29 +0530

    Day 10: Longest Consecutive Sequence brute force solution

---
+++ hashing/longest_consecutive_sequence/BruteForce.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public int longestConsecutive(int[] nums) {
+
+        if(nums.length == 0){
+            return 0;
+        }
+
+        int longest = 0;
+
+        for(int i = 0; i < nums.length; i++){
+
+            int current = nums[i];
+            int streak = 1;
+
+            while(arrayContains(nums, current + 1)){
+                current = current + 1;
+                streak = streak + 1;
+            }
+
+            longest = Math.max(longest, streak);
+        }
+
+        return longest;
+    }
+
+    // helper function
+    public boolean arrayContains(int[] nums, int target){
+        for(int num : nums){
+            if(num == target){
+                return true;
+            }
+        }
+        return false;
+    }
+}
