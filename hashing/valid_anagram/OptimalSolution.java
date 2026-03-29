c2ba5142fe979b5f4eda16398803724f2da28f2e
Aravind Bandi <aravind.bandi1845@gmail.com>
2026-03-29 21:18:59 +0530

    Day 15: Valid Anagram optimal solution using single frequency array

---
+++ hashing/valid_anagram/OptimalSolution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        int n = s.length();
+        int m = t.length();
+
+        if(n != m){
+            return false;
+        }
+
+        int[] counter = new int[26];
+
+        for(int i = 0; i < n; i++){
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+
+        for(int i = 0; i < 26; i++){
+            if(counter[i] != 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
