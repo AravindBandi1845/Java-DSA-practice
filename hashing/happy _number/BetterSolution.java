a70799735eaed16ed0d6c198e6bfd44fe8376ef5
Aravind Bandi <aravind.bandi1845@gmail.com>
2026-03-31 07:46:11 +0530

    Day 17: Happy Number solution using HashSet for cycle detection

---
+++ hashing/happy _number/BetterSolution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = helper(n);
+        }        
+        return n == 1;
+    }
+    static int helper(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            totalSum += digit * digit;
+            n /= 10;
+        }
+        return totalSum;
+    }
+}...
