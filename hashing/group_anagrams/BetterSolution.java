61369f67319520beee9bc2d1e65b0947614ec198
Aravind Bandi <aravind.bandi1845@gmail.com>
2026-04-04 08:20:05 +0530

    Day 21: Group Anagrams solution using sorting as key (HashMap)

---
+++ hashing/group_anagrams/BetterSolution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> result = new HashMap<>();
+
+        for(String s : strs){
+
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String sortedKey = new String(c);
+
+            result.putIfAbsent(sortedKey, new ArrayList<>());
+            result.get(sortedKey).add(s);
+        }
+
+        return new ArrayList<>(result.values());
+    }
+}
