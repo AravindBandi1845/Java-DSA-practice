dee52048dd0c5bd4984f85c6dafeeae5e5781551
Aravind Bandi <aravind.bandi1845@gmail.com>
2026-04-13 17:54:19 +0530

    Day 29: Implemented optimal one-pass solution for Second Largest Element in Array

---
+++ Arrays/second_largest_element/optimalSolution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int secondLargest(int[] nums) {
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for(int i = 0; i < nums.length; i++){
+
+            if(nums[i] > largest){
+                secondLargest = largest;
+                largest = nums[i];
+            }
+            else if(nums[i] < largest && nums[i] > secondLargest){
+                secondLargest = nums[i];
+            }
+        }
+
+        return (secondLargest == Integer.MIN_VALUE) ? -1 : secondLargest;
+    }
+}
